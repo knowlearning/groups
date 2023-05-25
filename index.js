7522671e8c4d9237efc50dff957ced8426e6d02b
Jason Ford <fordjason@gmail.com>
2023-05-25 15:05:11 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import { createStore } from 'vuex'
+import { browserAgent, vuePersistentStore } from '@knowlearning/agents'
+import mainComponent from './index.vue'
+import storeConfig from './store.js'
+
+window.Agent = browserAgent()
+
+const store = createStore(await vuePersistentStore(storeConfig))
+const app = createApp(mainComponent)
+app.use(store)
---
+++ index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="provider === null">initializing...</div>
+  <LoginMenu
+    v-else-if="provider === 'anonymous'"
+    google
+    microsoft
+  />
+  <div v-else>
+    logged in!
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style>
+  body
+  {
+    margin: 0;
+  }
+</style>
